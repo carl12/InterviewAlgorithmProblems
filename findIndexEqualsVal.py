d19704792daa02dd5a7abbfe9beb96ba85d65c6f
carl12 <carl.rodriguez13@gmail.com>
2018-07-18 12:05:44 -0700

    Add code for index equals value in sorted array problem

---
+++ findIndexEqualsVal.py
@@ -0,0 +1,40 @@
+import f_timer
+import random
+
+def index_equals_value_search(arr):
+  for i in range(len(arr)):
+    if i == arr[i]:
+      return i
+  return -1
+
+def index_equals_value_search2(arr):
+  # compare middle to middle index
+  # if index greater go down, vice versa
+  
+  lowest_found = len(arr)
+  left = 0
+  right = len(arr)-1
+
+  while(left <= right):
+    i = (left+right)//2
+    if arr[i] >= i :
+      if arr[i] == i and i < lowest_found:
+        lowest_found = i
+      right = i - 1
+    elif arr[i] < i:
+      left = i + 1
+
+  return lowest_found if lowest_found < len(arr) else -1
+
+
+def gen_sorted_rand_arr(minVal, maxVal, maxLength):
+
+  true_max_length = min(maxVal - minVal, maxLength)
+  length = random.randint(0,true_max_length)
+  nums = sorted(random.sample(range(minVal, maxVal), length))
+  return [nums]
+
+time,res = f_timer.get_func_time_and_res([index_equals_value_search, index_equals_value_search2], gen_sorted_rand_arr, [-500,10005,10000],1000)
+
+valid = [0 if a == -1 else 1 for a in res]
+print(sum(valid)/len(valid))
