21b8dd9106d07ea65af50301f626ff7e9665d86d
carl12 <carl.rodriguez13@gmail.com>
2018-07-19 16:44:49 -0700

    Add print statement for long run times of time funcs

--- f_timer.py
+++ f_timer.py
@@ -8,8 +8,12 @@ def get_func_time_and_res(functions, generatorFunc, generatorInput, iterations):
 
 	times = [[] for _ in functions]
 	results = [[] for _ in functions]
+	start = time.time()
 	for i in range(iterations):
 		funcInput = generatorFunc(*generatorInput)
+		if(time.time() - start > 6):
+			print([sum(t) for t in times],'On iteration',i,'of',iterations)
+			start = time.time()
 		for j,func in enumerate(functions):
 			run_time, res = time_func(func, funcInput)
 			times[j].append(run_time)
@@ -17,7 +21,7 @@ def get_func_time_and_res(functions, generatorFunc, generatorInput, iterations):
 
 		for j in range(len(functions)-1):
 			if results[j][i] != results[j+1][i]:
-				print(results[j][i],results[j+1][i],'results differ')
+				print(funcInput, results[j][i],results[j+1][i],'results differ')
 				return funcInput
 
 	return ([sum(func_times) for func_times in times], results[0])
