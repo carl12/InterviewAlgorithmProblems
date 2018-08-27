b19f9dbb91007b472339d2ef1dc624380c579e67
carl12 <carl.rodriguez13@gmail.com>
2018-08-27 16:33:32 -0700

    Used shallow copied input for functions. Add print for time spent outside functions in f_timer

--- f_timer.py
+++ f_timer.py
@@ -1,5 +1,5 @@
 import time
-
+import copy
 def time_funcs(functions, generatorFunc, generatorInput, iterations):
 	return get_func_time_and_res(functions, generatorFunc, generatorInput, iterations)[0]
 
@@ -9,21 +9,27 @@ def get_func_time_and_res(functions, generatorFunc, generatorInput, iterations):
 	times = [[] for _ in functions]
 	results = [[] for _ in functions]
 	start = time.time()
+	last = start
 	for i in range(iterations):
 		funcInput = generatorFunc(*generatorInput)
-		if(time.time() - start > 6):
+		if(time.time() - last > 6):
 			print([sum(t) for t in times],'On iteration',i,'of',iterations)
-			start = time.time()
+			last = time.time()
 		for j,func in enumerate(functions):
-			run_time, res = time_func(func, funcInput)
+			run_time, res = time_func(func, copy.deepcopy(funcInput))
 			times[j].append(run_time)
 			results[j].append(res)
 
 		for j in range(len(functions)-1):
 			if results[j][i] != results[j+1][i]:
-				print(funcInput, results[j][i],results[j+1][i],'results differ')
+				outputs = ""
+				for res in results:
+					outputs += str(res[i]) + " "
+				print(funcInput, outputs, 'results differ')
 				return funcInput
-
+	finish = time.time()
+	if(finish - start > 3):
+		print(finish - start - sum([sum(func_times) for func_times in times]), ' is non-func processing time out of total ', finish - start)
 	return ([sum(func_times) for func_times in times], results[0])
 
 
