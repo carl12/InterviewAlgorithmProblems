1b796d38b1e1b4b42aa1fc4bb9ec5ee5c18b3b19
carl12 <carl.rodriguez13@gmail.com>
2018-08-27 16:39:06 -0700

    Update flip word text

--- flipWordOrder.py
+++ flipWordOrder.py
@@ -1,5 +1,9 @@
 '''
-Problem: given an array of characters, reverse the order of the words in the array
+Sentence Reverse
+
+You are given an array of characters arr that consists of sequences of characters separated by space characters. Each space-delimited sequence of characters defines a word.
+
+Implement a function reverseWords that reverses the order of the words in the array in the most efficient manner.
 
 '''
 import random
