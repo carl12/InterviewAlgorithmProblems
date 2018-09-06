d5747c5a7d27c68c0f1b62af9e17958e1a27f8d3
carl12 <carl.rodriguez13@gmail.com>
2018-09-05 20:49:32 -0700

    Add problem descriptions to problems

--- findIndexEqualsVal.py
+++ findIndexEqualsVal.py
@@ -1,3 +1,9 @@
+'''
+Given a sorted array arr of distinct integers, write a function 
+indexEqualsValueSearch that returns the lowest index i for which 
+arr[i] == i. Return -1 if there is no such index. Analyze the time 
+and space complexities of your solution and explain its correctness.
+'''
 import f_timer
 import random
 
--- pancakeSort.py
+++ pancakeSort.py
@@ -1,3 +1,19 @@
+'''
+Given an array of integers arr:
+
+    Write a function flip(arr, k) that reverses the order of the first k 
+    elements in the array arr.
+    Write a function pancakeSort(arr) that sorts and returns the input array. 
+    You are allowed to use only the function flip you wrote in the first step 
+    in order to make changes in the array.
+
+Analyze the time and space complexities of your solution.
+
+Note: it’s called pancake sort because it resembles sorting pancakes on a 
+plate with a spatula, where you can only use the spatula to flip some of the 
+top pancakes in the plate. To read more about the problem, see the Pancake 
+Sorting Wikipedia page.
+'''
 def pancake_sort(arr):
   for i in range(len(arr)):
     
